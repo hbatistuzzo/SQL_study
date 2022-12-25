989b9d85930667ae3a4b081946bf30a20d902c98
HBatistuzzo <hbatistuzzo@gmail.com>
2022-12-24 23:56:21 -0300

    reorg

--- study_techTFQ.sql
+++ study_techTFQ.sql
@@ -315,13 +315,55 @@ WHERE flag = 'YES';
 -- 8)
 
 
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- 9)
+drop table event_category;
+create table event_category
+(
+  event_name varchar(50),
+  category varchar(100)
+);
+
+drop table physician_speciality;
+create table physician_speciality
+(
+  physician_id int,
+  speciality varchar(50)
+);
+
+drop table patient_treatment;
+create table patient_treatment
+(
+  patient_id int,
+  event_name varchar(50),
+  physician_id int
+);
 
 
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- 10)
+insert into event_category values ('Chemotherapy','Procedure');
+insert into event_category values ('Radiation','Procedure');
+insert into event_category values ('Immunosuppressants','Prescription');
+insert into event_category values ('BTKI','Prescription');
+insert into event_category values ('Biopsy','Test');
+
+
+insert into physician_speciality values (1000,'Radiologist');
+insert into physician_speciality values (2000,'Oncologist');
+insert into physician_speciality values (3000,'Hermatologist');
+insert into physician_speciality values (4000,'Oncologist');
+insert into physician_speciality values (5000,'Pathologist');
+insert into physician_speciality values (6000,'Oncologist');
+
+
+insert into patient_treatment values (1,'Radiation', 1000);
+insert into patient_treatment values (2,'Chemotherapy', 2000);
+insert into patient_treatment values (1,'Biopsy', 1000);
+insert into patient_treatment values (3,'Immunosuppressants', 2000);
+insert into patient_treatment values (4,'BTKI', 3000);
+insert into patient_treatment values (5,'Radiation', 4000);
+insert into patient_treatment values (4,'Chemotherapy', 2000);
+insert into patient_treatment values (1,'Biopsy', 5000);
+insert into patient_treatment values (6,'Chemotherapy', 6000);
+
+
+select * from patient_treatment;
+select * from event_category;
+select * from physician_speciality;
